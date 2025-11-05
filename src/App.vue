877bf59ba4675b1ea39f6948789d30b952107d1c
Jeff James <jeffjames@gmail.com>
2025-11-05 11:22:16 -0800

    Home page update

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    Welcome to the future online home of Team Squander!
+  </div>
+  <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
