7a98bb0bf9f0f099b51a7bfcf6bde887eca3ce21
Jeff James <jeffjames@gmail.com>
2025-11-05 17:26:31 -0800

    Delete demo code

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <h1>Team Squander!</h1>
     <img class="full-width" src="https://teamsquander.s3.us-west-2.amazonaws.com/SquanderFinal2024resizedTP.jpg" />
   </div>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
