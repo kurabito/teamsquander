a8eec8106394f2d1d1b9cf6c59b2ac6576dd0cbb
Jeff James <jeffjames@gmail.com>
2025-11-10 16:59:37 -0800

    Align elements to top instead of center

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,13 +3,13 @@
     <picture>
       <source srcset="https://teamsquander.s3.us-west-2.amazonaws.com/SquanderFinal2024resizedTP.jpg" media="(prefers-color-scheme: light)">
       <source srcset="https://teamsquander.s3.us-west-2.amazonaws.com/SquanderFinal2024resizedTPInverse.jpg" media="(prefers-color-scheme: dark)">
-      <img class="full-width" src="https://teamsquander.s3.us-west-2.amazonaws.com/SquanderFinal2024resizedTP.jpg" alt="Logo" />
+      <img src="https://teamsquander.s3.us-west-2.amazonaws.com/SquanderFinal2024resizedTP.jpg" alt="Logo" />
     </picture>
   </div>
 </template>
 
-<style>
-.full-width {
+<style scoped>
+img {
   max-width: 100%; /* Image will not exceed its container's width */
   height: auto; /* Height adjusts proportionally to maintain aspect ratio */
 }
