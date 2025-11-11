3f102a4229b27cad874cfbd47345e1eca1777e6f
Jeff James <jeffjames@gmail.com>
2025-11-11 12:44:15 -0800

    Add Merch link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/discs">Discs</router-link>
+      <router-link to="/discs">Discs</router-link> |
+      <a href="https://www.teepublic.com/t-shirt/65580537-team-squander?store_id=647266" target="_blank">Merch</a>
     </nav>
     <router-view></router-view>
   </div>
